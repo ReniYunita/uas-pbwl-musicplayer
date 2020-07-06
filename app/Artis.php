4a4d9d7ebcaa21c14f51f58bd2c93f595fc4d5fe
reniyunita <reniyunita045@gmail.com>
2020-07-06 15:30:33 +0700

    Upload UAS PBWL

---
+++ app/Artis.php
@@ -0,0 +1,10 @@
+<?php
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+
+class Artis extends Model
+{
+    protected $table = "tb_artis";
+    protected $primaryKey = 'artis_id';
+    protected $fillable = ['nama_artis'];
+}
---
+++ database/migrations/2020_07_03_074952_create_tb_albums_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTbAlbumsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('tb_albums', function (Blueprint $table) {
+            $table->increments('album_id');
+            $table->string('nama_album');
+            $table->unsignedInteger('artis_id');
+            $table->foreign('artis_id')->references('artis_id')->on('tb_artis');
+            $table->timestamps();
+        });
+    }
+    public function down()
+    {
+        Schema::dropIfExists('tb_albums');
+    }
+}
